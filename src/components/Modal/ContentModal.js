b59e4391fd27c66d0febc752ac4efad6e591c098
swapnali4397 <swapnalidukhande98@gmail.com>
2022-11-01 15:46:54 +0530

    seventh commit

--- src/components/Modal/ContentModal.js
+++ src/components/Modal/ContentModal.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import * as React from "react";
+import React from "react";
 import Modal from "@mui/material/Modal";
 import Backdrop from "@mui/material/Backdrop";
 import Fade from "@mui/material/Fade";
